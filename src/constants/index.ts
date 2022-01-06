e603df0253a6cef6f394e66aa875f43afd9a2d3f
João Bordalo <joao.bordalo@gmail.com>
2022-01-06 13:07:53 +0000

    adds object to store asset hashes (mainnet and testnet)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -64,29 +64,51 @@ export const enum AssetTickers {
   USDT = 'USDT',
 }
 
+interface IAssetHash {
+  [network: string]: string;
+}
+
+export const AssetHashes: Record<string, IAssetHash> = {
+  [AssetNames.BTSE]: {
+    [NetworkNames.MAINNET]: 'b00b0ff0b11ebd47f7c6f57614c046dbbd204e84bf01178baf2be3713a206eb7',
+  },
+  [AssetNames.LBTC]: {
+    [NetworkNames.MAINNET]: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d',
+    [NetworkNames.TESTNET]: '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49',
+  },
+  [AssetNames.LCAD]: {
+    [NetworkNames.MAINNET]: '0e99c1a6da379d1f4151fb9df90449d40d0608f6cb33a5bcbfc8c265f42bab0a',
+    [NetworkNames.TESTNET]: 'ac3e0ff248c5051ffd61e00155b7122e5ebc04fd397a0ecbdd4f4e4a56232926',
+  },
+  [AssetNames.USDT]: {
+    [NetworkNames.MAINNET]: 'ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2',
+    [NetworkNames.TESTNET]: 'f3d1ec678811398cd2ae277cbe3849c6f6dbd72c74bc542f7c4b11ff0e820958',
+  },
+}
+
 // ASSETS is an array of "featured assets"
 // (i.e assets data harcoded in the app, it lets to save some network requests for very common assets)
 export const ASSETS: Coin[] = [
   {
-    assetHash: LIQUID_BTC,
+    assetHash: AssetHashes[AssetNames.LBTC][NetworkNames.MAINNET],
     name: AssetNames.LBTC,
     precision: PRECISION,
     ticker: AssetTickers.LBTC,
   },
   {
-    assetHash: LIQUID_USDT,
+    assetHash: AssetHashes[AssetNames.USDT][NetworkNames.MAINNET],
     name: AssetNames.USDT,
     precision: PRECISION,
     ticker: AssetTickers.USDT,
   },
   {
-    assetHash: LIQUID_CAD,
+    assetHash: AssetHashes[AssetNames.LCAD][NetworkNames.MAINNET],
     name: AssetNames.LCAD,
     precision: PRECISION,
     ticker: AssetTickers.LCAD,
   },
   {
-    assetHash: 'b00b0ff0b11ebd47f7c6f57614c046dbbd204e84bf01178baf2be3713a206eb7',
+    assetHash: AssetHashes[AssetNames.BTSE][NetworkNames.MAINNET],
     name: AssetNames.BTSE,
     precision: PRECISION,
     ticker: AssetTickers.BTSE,

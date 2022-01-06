1bc8c0d133fbfec0d0b4e961c77af9767c5d2926
João Bordalo <joao.bordalo@gmail.com>
2022-01-06 13:14:52 +0000

    show CoinLogo based on coin name instead of coin hash

--- src/components/CoinRow.svelte
+++ src/components/CoinRow.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { LIQUID_BTC, LIQUID_CAD, LIQUID_USDT } from '../constants';
+  import { AssetNames } from '../constants';
 
   import BitcoinLogo from '../assets/images/liquid-btc.svg';
   import TetherLogo from '../assets/images/liquid-tether.svg';
@@ -7,13 +7,13 @@
   import QuestionMark from '../assets/images/question-mark.svg';
 
   const CoinLogo = {
-    [LIQUID_BTC]: BitcoinLogo,
-    [LIQUID_USDT]: TetherLogo,
-    [LIQUID_CAD]: LCADLogo,
+    [AssetNames.LBTC]: BitcoinLogo,
+    [AssetNames.LCAD]: LCADLogo,
+    [AssetNames.USDT]: TetherLogo,
   };
 
   const getImgSource = (coin) => {
-    const logo = CoinLogo[coin.assetHash];
+    const logo = CoinLogo[coin.name];
     if (!logo) return QuestionMark;
     return logo;
   };
